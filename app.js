4b2aefe017a40d77e2a2b64c0505b67d0dc5044c
Anthony Muratore <amurato1@binghamton.edu>
2016-11-05 22:19:17 -0400

    more changes

--- app.js
+++ app.js
@@ -54,6 +54,7 @@
 	  el.appendChild(document.createTextNode(
 		  'Could not find polling place for ' + normalizedAddress));
 	}
+	createTable();
 	}
 
 	/**
@@ -69,12 +70,9 @@
 		var table = document.createElement('table');
 		var row1 = table.insertRow(0);
 		var row1col1 = row1.insertCell(0);
-		row1col1.innerHTML = 'Col1';
+		row1col1.innerHTML = 'Democrat';
 		var row1col2 = row1.insertCell(1);
-		row1col2.innerHTML = 'Col2';
-		// Insert New Column for Row1 at index '2'.
-		var row1col3 = row1.insertCell(2);
-		row1col3.innerHTML = 'Col3';
+		row1col2.innerHTML = 'Republican';
 		// Append Table into div.
 		var div = document.getElementById('contestTable');
 		div.appendChild(table);
